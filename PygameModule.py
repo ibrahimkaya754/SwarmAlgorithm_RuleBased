5ed474d810b72567384b88044631e19a4d1b8ed4
ibrahimkaya754 <ibrahimkaya754@gmail.com>
2020-02-18 23:42:44 +0300

    Add files via upload

---
+++ PygameModule.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 13:25:16 2017
+
+@author: roketsan
+"""
+import numpy as np
+import pygame
+import random
+
+class particle(pygame.sprite.Sprite): # CUSTOMIZABLE OBJECT
+    def __init__(self, screen, background, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.screen = screen
+        self.background = background
+        if color == 0:
+            color1 = 200
+            color2 = 100
+            color3 = 50 
+        elif color == 1:
+            color1 = 200
+            color2 = 100
+            color3 = 50 
+        elif color == 2:
+            color1 = 10
+            color2 = 10
+            color3 = 10            
+        self.image = pygame.Surface((4, 4))
+        self.image.fill((255, 255, 255))
+        pygame.draw.circle(self.image, (color1, color2, color3), (2, 2), 2) 
+        self.rect = self.image.get_rect()       
+        
+        self.positionx      = random.random() * (200)
+        self.positiony      = random.random() * (650)
+#        self.positionx      = random.randint(0,1500)
+#        self.positiony      = random.randint(0,750)
+        self.rect.center    = (self.positionx, self.positiony)
+        self.vel_min        = -20
+        self.vel_max        = 20
+        self.lower_boundary = np.array([-1500,-1500])
+        self.upper_boundary = np.array([1500,1500])
+        self.velx           = np.random.rand()
+        self.vely           = np.random.rand()
+        self.dx             = random.random() * 20
+        self.dy             = random.random() * 20
+    
+    
+    def update(self): 
+        oldCenter = self.rect.center
+#        self.rect.centerx += self.dx #x_hareketi
+#        self.rect.centery += self.dy #y_hareketi
+        self.positionx      = self.positionx + self.dx
+        self.positiony      = self.positiony + self.dy
+        self.rect.center    = (self.positionx, self.positiony)
+#        pygame.draw.line(self.background, (0, 0, 0), oldCenter, self.rect.center) # iz        
+        self.checkBounds()        
+    
+    def checkBounds(self):
+        """ bounce on encountering any screen boundary """
+        
+        if self.rect.right >= self.screen.get_width():
+            self.dx *= -1
+        if self.rect.left <= 0:
+            self.dx *= -1
+        if self.rect.bottom >= self.screen.get_height():
+            self.dy *= -1
+        if self.rect.top  <= 0:
+            self.dy *= -1
+            
+        
+###################### FIND DISTANCES ###################################################################################################
+class distance():
+    def __init__(self,population_number,dimension,distances,
+                 distances_abs,position,closest_distances_abs,
+                 closest_particles_abs,closestneighbours):
+        self.population_number      = population_number
+        self.dimension              = dimension
+        self.distances              = distances
+        self.distances_abs          = distances_abs
+        self.position               = position
+        self.closest_distances_abs  = closest_distances_abs
+        self.closest_particles_abs  = closest_particles_abs
+        self.closestneighbours      = closestneighbours
+
+    def find_distances(self):
+        self.closest_neighbours  = np.zeros((self.population_number,self.population_number))
+        for jj in range(self.dimension):
+            for ii in range(self.population_number):
+                for kk in range(self.population_number):
+                    self.dim               = jj * self.population_number + kk
+                    self.distances[ii,self.dim] = self.position[ii,jj] - self.position[kk,jj]
+
+        for ii in range(self.population_number):
+            for kk in range(self.population_number):
+                column1                   = kk
+                column2                   = self.population_number + kk
+                self.distances_abs[ii,kk] = np.sqrt(self.distances[ii,column1]**2 + self.distances[ii,column2]**2)
+
+        for ii in range(self.population_number):
+            self.closest_distances_abs[ii,:]       = np.sort(self.distances_abs[ii,:])
+            self.closest_particles_abs[ii,:]       = np.argsort(self.distances_abs[ii,:])
+
+        for ii in range(self.population_number):
+            for kk in range(self.population_number):
+                self.closest_neighbours[ii,kk] = self.closest_distances_abs[ii,kk] < 30     # 30 m has been chosen randomly, must be optimized later.
+
+        for ii in range(self.population_number):
+            self.closestneighbours[ii]   = 0
+            for kk in range(self.population_number):
+                if self.closest_neighbours[ii,kk] == True:
+                    self.closestneighbours[ii] = self.closestneighbours[ii] + 1
+                else:
+                    break
+        
+        return self.distances,self.distances_abs,self.closest_distances_abs,self.closest_particles_abs,self.closestneighbours
+
+    def distance_to_waypoint(self,waypoint):
+        number_of_particles = self.population_number - 1
+        dist_to_wp = np.zeros((number_of_particles,1))
+        for ii in range(number_of_particles):
+            dist             = self.position[ii,:] - waypoint
+            dist_to_wp[ii,0] = np.sqrt(dist[0]**2 + dist[1]**2)
+        return dist_to_wp      
+
+    def dist_to_wypnt(self,pos,waypoint):
+        dist       = pos - waypoint
+        dist_to_wp = np.sqrt(dist[0]**2 + dist[1]**2)
+        return dist_to_wp  
+        
+###########################################################################################################################################
---
+++ Swarm_Algorithm_RuleBased.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 09:50:14 2020
+
+@author: ibrahimkaya
+
+The Rulebased Algorithm has been taken from the great paper 
+"""
+import numpy as np
+import math
+
+class swarm_algorithm():
+    def __init__(self,params):
+        self.params                     = params
+        
+    def algo(self,particle,axes,position,velocity,closest_particles_abs,
+             xtrg,wght,distance_to_target,vflock= 20,delta_t= 0.10,
+             vel_min= -20.0,vel_max= 20.0):
+        
+        r0,r1,r2,D,Cfrict,Cshill,R,d    = [self.params[ii] for ii in range(8)]
+        apot, aslp, v1, a1, a6          = [0 for ii in range(5)]
+    
+        self.particle                   = particle
+        self.axes                       = axes
+        self.position                   = position
+        self.velocity                   = velocity
+        self.closest_particles_abs      = closest_particles_abs
+        self.xtrg                       = xtrg
+        self.wght                       = wght
+        self.distance_to_target         = distance_to_target
+        self.closestneighbour_number = 3
+        for nb in range(1,self.closestneighbour_number):
+            if np.abs(self.position[self.closest_particles_abs[self.particle,nb],self.axes] - self.position[self.particle,self.axes]) < r0:
+                apot = apot + D * np.minimum(r1,r0 - np.abs(self.position[self.closest_particles_abs[self.particle,nb],self.axes] - \
+                       self.position[self.particle,self.axes])) * (self.position[self.closest_particles_abs[self.particle,nb],self.axes]  - \
+                       self.position[self.particle,self.axes]) / np.abs(self.position[self.closest_particles_abs[self.particle,nb],self.axes] - \
+                       self.position[self.particle,self.axes])
+            else:
+                apot = apot + 0
+
+            aslp = aslp + Cfrict * (self.velocity[self.closest_particles_abs[self.particle,nb],self.axes] - \
+                   self.velocity[self.particle,self.axes]) / (np.maximum(np.abs(self.position[self.closest_particles_abs[self.particle,nb],self.axes] - \
+                   self.position[self.particle,self.axes])-(r0-r2),1))**2
+
+            a1   = a1 + (self.position[self.closest_particles_abs[self.particle,nb],self.axes] - \
+                   self.position[self.particle,self.axes]) / self.closestneighbour_number
+
+            a6   = a6 + self.xtrg[self.axes] - self.position[self.particle,self.axes]
+
+            v1   = v1 + (self.velocity[self.closest_particles_abs[self.particle,nb],self.axes] - \
+                   self.velocity[self.particle,self.axes]) / self.closestneighbour_number
+
+        vspp   = vflock * self.velocity[self.particle,self.axes] / np.abs(self.velocity[self.particle,self.axes])
+        awall  = Cshill * self.smooth_transfer_function(np.abs(self.xtrg[self.axes] - \
+                self.position[self.particle,self.axes]),R,d) * (vflock * ((self.xtrg[self.axes] - \
+                self.position[self.particle,self.axes]) / np.abs(self.xtrg[self.axes] - \
+                self.position[self.particle,self.axes])) - self.velocity[self.particle,self.axes])
+        vtrack = 0
+        vel    = self.velocity[self.particle,self.axes]
+        
+        delta_vel = ((self.wght[5]) * (v1+vspp+vtrack-self.velocity[self.particle,self.axes]) + \
+                    (self.wght[0] * apot + self.wght[1] * aslp + self.wght[2] * awall + self.wght[3] * a1 + self.wght[4] * a6)) * delta_t
+
+        self.velocity[self.particle,self.axes] = self.velocity[self.particle,self.axes] + delta_vel
+        self.velocity[self.particle,self.axes] = self.distance_to_target/100 * self.velocity[self.particle,self.axes]
+        
+        if math.isinf(self.velocity[self.particle,self.axes]):
+            self.velocity[self.particle,self.axes] = vel
+        if self.velocity[self.particle,self.axes]>vel_max:
+            self.velocity[self.particle,self.axes] = vel_max
+        elif self.velocity[self.particle,self.axes]<vel_min:
+            self.velocity[self.particle,self.axes] = vel_min
+        
+        self.position_delta = self.velocity[self.particle,self.axes] * delta_t
+    
+    ######################### SMOOTH TRANSFER FUNCTION ########################################################################################
+    def smooth_transfer_function(self,x,R,d):
+        if x<=R:
+            res = 0
+        elif x>R and x<=R+d:
+            res = (np.sin(np.pi/d*(x-R) - np.pi/2) + 1) / 2
+        else:
+            res = 1
+        return res
